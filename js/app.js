0f96a7d9b4556b81df244feadece0d9573f07466
unknown <forinstagram9887@gmail.com>
2024-01-27 20:46:54 +0500

    first js project

---
+++ js/app.js
@@ -0,0 +1,33 @@
+// let age = 19;
+
+// if(age >= 18) {
+// 	console.log("Xaydovchilik guvohnomasini olsangiz bo'ladi!")
+// } else {
+// 	let yearsLeft = 18 - age;
+// 	console.log(`Hali yosh ekansiz ${yearsLeft} yil kutib turing :)`)
+// }
+
+
+// let birthYear = 1998
+// let century;
+
+// if(birthYear >= 2000) {
+// 	century = 20;
+// } else {
+// 	century = 21;
+
+let count = 0;
+
+        function updateCounter() {
+            document.getElementById("counter").innerText = count;
+        }
+
+        function increment() {
+            count++;
+            updateCounter();
+        }
+
+        function decrement() {
+            count--;
+            updateCounter();
+        }
